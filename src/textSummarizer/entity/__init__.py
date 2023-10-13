89ba041f72f4f33e16b4600ae76b61e3d0d167c2
Jorge Rivero <jlrivero85@gmail.com>
2023-10-13 13:13:02 -0400

    Data Transformation and Model Trainer

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import List
 
+
 @dataclass(frozen=True)
 class DataIngestionConfig:
     root_dir: Path
@@ -17,4 +18,24 @@ class DataValidationConfig:
     all_required_files: List
 
 
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
+    tokenizer_name: Path
 
+
+@dataclass(frozen=True)
+class ModelTrainerConfig:
+    root_dir: Path
+    data_path: Path
+    model_ckpt: Path
+    num_train_epochs: int
+    warmup_steps: int
+    per_device_train_batch_size: int
+    weight_decay: float
+    logging_steps: int
+    evaluation_strategy: str
+    eval_steps: int
+    save_steps: float
+    gradient_accumulation_steps: int
